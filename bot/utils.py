4b78e373a151963d77d206f1ce42135197c4cfd2
navruzoffd <navruzovdaniyal04@gmail.com>
2024-10-23 17:08:29 +0300

    update

--- bot/utils.py
+++ bot/utils.py
@@ -15,7 +15,7 @@ def decode_referral_id(encoded_id: str) -> int:
 def gen_x_link(hash_id):
     text = f"Check out $RIKO, the fearless pet of GRA.FUN! Join the community and earn points with my referral link! 👇 \nhttps://t.me/adtgtestbot?start={hash_id}\nBSC is ours for the taking! 🏴‍☠️"
     encoded_text = urllib.parse.quote(text, safe='')
-    link = "https://x.com/intent/post?text="+encoded_text
+    link = "https://twitter.com/intent/tweet?text="+encoded_text
     return link
 
 def start_msg():
